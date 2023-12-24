8a659c2b48cc92a1e72ce896a39b4fd663e34188
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-24 20:32:03 +0300

    Update movie.service.ts

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-const API_URL ="http://www.omdbapi.com/?i=tt3896198&apikey=cac7a121";
+const API_URL ="http://www.omdbapi.com/?apikey=cac7a121&";
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +9,6 @@ export class MovieService {
   constructor(private http:HttpClient) { }
 
   getMoviesWithTitle(title:string){
-    return this.http.get(`${API_URL}/?t=${title}`)
+    return this.http.get(`${API_URL}t=${title}`)
   }
 }
