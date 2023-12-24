f4c8ba06d557e0d92ce6e80fd5041ba7bd60c940
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-24 19:18:35 +0300

    initial commit

---
+++ src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-list.component.html',
+})
+export class MovieListComponent {
+
+}
