2eddf74d5889088a5c94ad9393d87a7c88e60823
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-24 21:46:52 +0300

    update to movie card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from '../shared/modules/Movie';
+import { Episode } from '../shared/modules/Episode';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'movie-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './movie-card.component.html',
 })
 export class MovieCardComponent {
-  @Input() movie!:Movie;
+  @Input() title!: string;
+  @Input() imdbRating?: string;
+  @Input() poster?: string;
+  @Input() episode?: number;
+  @Input() season?: number;
+  @Input() totalSeasons?: number;
+  @Input() released?: Date;
 } 
