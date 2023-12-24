26342d84c8c932a8dc40664c8f732d9be63f14cf
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-24 21:47:08 +0300

    Getting datas based on given inputs

--- src/app/movie-app/movie-app.component.ts
+++ src/app/movie-app/movie-app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MovieService } from '../shared/services/movie.service';
 import { Movie } from '../shared/modules/Movie';
 import { MovieListComponent } from "../movie-list/movie-list.component";
+import { Series } from '../shared/modules/Series';
+import { Episode } from '../shared/modules/Episode';
 @Component({
     selector: 'movie-app',
     standalone: true,
@@ -10,15 +12,36 @@ import { MovieListComponent } from "../movie-list/movie-list.component";
     imports: [FormsModule,ReactiveFormsModule, MovieListComponent]
 })
 export class MovieAppComponent {
-  movies:Movie[] = [];
+  movie?:Movie;
+  series?:Series;
   constructor(private movieService:MovieService){}
-
-  title = new FormControl('',[Validators.required,Validators.minLength(3)]);
+  formBuilder = new FormBuilder().nonNullable;
+  formGroup = this.formBuilder.group(
+    {
+      title: this.formBuilder.control('',[Validators.required,Validators.minLength(3)]),
+      season: this.formBuilder.control<number|undefined>(undefined,[Validators.min(1)]),
+      episode: this.formBuilder.control<number|undefined>(undefined,[Validators.min(1)])
+    }
+  )
   getMoviesViaTitle(){
-    if(this.title.valid){
-      this.movieService.getMoviesWithTitle(this.title.value!).subscribe((data:any)=>{
+    if(this.formGroup.get("title")!.valid){
+      let title = this.formGroup.controls.title.value;
+      let season = this.formGroup.controls.season.value;
+      let episode = this.formGroup.controls.episode.value;
+      this.movieService.getMoviesWithTitle(title!,season,episode).subscribe((data:any)=>{
         console.log(data);
-        this.movies = [data];
+        if(episode){
+          this.movie = data;  
+          this.series = undefined;
+        }  
+        else if(season){
+          this.series = data;
+          this.movie = undefined;
+        }
+        else{
+          this.movie = data;
+          this.series = undefined;
+        }
       })
     }
   }
